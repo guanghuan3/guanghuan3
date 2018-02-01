fdfa1dae31c19841ddf488aeda88595b829239ba
Terry <guanghuan3@163.com>
2018-02-01 23:19:33 +0800

    [author]:Terry [project]:guanghuan3 [description]:add parent pom

--- customer/src/main/java/com/guanghuan3/configurations/components/ComponentsConfig.java
+++ customer/src/main/java/com/guanghuan3/configurations/components/ComponentsConfig.java
@@ -15,7 +15,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @Configuration
 // core包中的内置实现，基础框架使用时可以使用，也可以自己实现接口定制
 @MapperScan(basePackages = {"com.zwb.common.business", "com.guanghuan3.customer"})
-@ComponentScan(basePackages = "com.zwb.commons.business")
+@ComponentScan(basePackages = "com.zwb.common.business")
 @ServletComponentScan("com.guanghuan3.web")
 public class ComponentsConfig
 {
